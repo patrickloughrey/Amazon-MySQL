f1f011d1484c6e152407c2aaa01535d7e8321207
Patrick Loughrey <PatrickLoughrey@Patricks-MBP-2.home>
2017-02-23 16:56:45 -0500

    SQL Queries working with inquirer

--- js/index.js
+++ js/index.js
@@ -1,35 +1,44 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
-inquirer.prompt([
-  { type: "input",
-    name: "database",
-    message: "What database do you want to connect to?"}
-]).then(function(data) {
-    var connection = mysql.createConnection({
+var connection = mysql.createConnection({
         host      : 'localhost',
         user      : 'root',
         password  : '',
-        database  : data.database,
-    });
-
-    connection.connect();
-
-    function selectTable(table) {
-        connection.query('SELECT * FROM ' + table, function(error, results, fields) {
-            if(error) throw error;
-            console.log("The solution is " + results);
-        });
-    }
-
-    inquirer.prompt([
-        { type: "input",
-          name: "table", 
-          message: "What table do you want to select from?" }
-    ]).then(function(data) {
-          selectTable(data.table);
-
-    });
-
-    /* End of database connection */
+        database  : 'bamazon_db',
 });
+
+connection.connect();
+
+function selectTable(table) {
+  connection.query('SELECT * FROM ' + table, function(error, results, fields) {
+      if(error) throw error;
+      console.log(results);
+  });
+}
+
+connection.query('SELECT * FROM products ', function(error, results, fields) {
+  console.log(results);
+  console.log("\n");
+  inquirer.prompt([
+    { type: "input",
+      name: "product_id", 
+      message: "Enter the ID of the product you would like to purchase." }
+
+  ]).then(function(data) {
+        console.log(data.product_id);
+  });
+
+  inquirer.prompt([
+    { type: "input",
+      name: "stock_quantity", 
+      message: "How many units of the product which you like to purchase?" }
+
+  ]).then(function(data) {
+        console.log(data.product_id);
+  });
+
+      
+});
+
+/* End of database connection */
