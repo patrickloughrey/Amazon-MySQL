51b45969ba654429c849e28ce090d8e11efd6798
Patrick Loughrey <PatrickLoughrey@nwk--192-180.nwk.ruw.rutgers.edu>
2017-02-24 18:07:28 -0500

    Created product table for aesthetics

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var Table = require('cli-table');
 
 var connection = mysql.createConnection({
         host      : 'localhost',
@@ -10,35 +11,92 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-function selectTable(table) {
-  connection.query('SELECT * FROM ' + table, function(error, results, fields) {
-      if(error) throw error;
-      console.log(results);
-  });
-}
 
+/* Create table so SQL response is not in RowDataPacket form */
 connection.query('SELECT * FROM products ', function(error, results, fields) {
-  console.log(results);
-  console.log("\n");
+    
+    /* Create a table */
+    var table = new Table({
+        head : ['Id', 'Product', 'Price']
+    });
+
+    var i = 0;
+    while(i < results.length) {
+        table.push([results[i].id, results[i].product_name, results[i].price]);
+        i++;
+    }
+    console.log(table.toString());
+    console.log("\n");
+    console.log("\n");
+
+/* Ask customer if they would like to make a purchase */
+inquirer.prompt([
+    { type: "input",
+      name: "purchase", 
+      message: "Would you like to make a purchase? (yes/no)" }
+
+  ]).then(function(data) {
+
+        if(data.purchase == 'yes') {
+            purchase();
+
+        } else if(data.purchase == 'no') {
+            console.log("Come back any time!");
+
+        } else {
+            console.log("Sorry, I did not understand your response.");
+        }
+
+});
+
+/* Function to handle purchase requests */
+function purchase() { 
+
+  /* Prompt user for the ID of the product they wish to purchase */
   inquirer.prompt([
     { type: "input",
       name: "product_id", 
       message: "Enter the ID of the product you would like to purchase." }
 
   ]).then(function(data) {
-        console.log(data.product_id);
+
+        var product = data.product_id;
+
+        /* Prompt user for quantity of product they wish to purchase */
+        connection.query('SELECT * FROM products ', function(error, results, fields) {
+            console.log("\n");
+            inquirer.prompt([
+                { type: "input",
+                  name: "stock_quantity",
+                  message: "How many units of the product would you like to purchase?" }
+
+            ]).then(function(data) {
+
+                  var quantity = data.stock_quantity;
+                  var currItem = results[product - 1];
+                  var newStock = currItem.stock_quantity - quantity;
+                  var purchaseAmount = quantity * currItem.price;
+                  console.log(quantity);
+                  console.log(currItem);
+                  console.log(newStock);
+                  console.log(purchaseAmount);
+                  /*
+                  connection.query('INSERT INTO sales SET ? ', function(error, results, fields) {
+                      product_id : product,
+                      quantity_purchased : stock 
+
+                  }, function(error, results, fields) {
+                        console.log("Insert successful!");
+                  });
+
+            }); */
+        });
+
+    });
+
   });
 
-  inquirer.prompt([
-    { type: "input",
-      name: "stock_quantity", 
-      message: "How many units of the product which you like to purchase?" }
+} /********* End of purchase() **************/
 
-  ]).then(function(data) {
-        console.log(data.product_id);
-  });
+}); /********* End of products connection.query() *********/
 
-      
-});
-
-/* End of database connection */
