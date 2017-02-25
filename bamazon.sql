26bb8a643100d7e2a4501be54ac0e26e28e858c0
Patrick Loughrey <PatrickLoughrey@Patricks-MBP-2.home>
2017-02-25 01:50:56 -0500

    SQL Dump Successful

--- bamazon.sql
+++ bamazon.sql
@@ -1,60 +1,111 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for osx10.12 (x86_64)
 --
--- Table structure for table departments 
---
-CREATE TABLE departments (
-  id INTEGER AUTO_INCREMENT NOT NULL,
-  department_name VARCHAR(20),
-  over_head_costs DECIMAL(10,4),
-  PRIMARY KEY (id)
-);
+-- Host: localhost    Database: bamazon_db
+-- ------------------------------------------------------
+-- Server version	5.7.17
 
-INSERT INTO departments (department_name, over_head_costs) VALUES 
-("Technology", 8000),
-("Home Improvement", 1000),
-("Automotive", 10000);
-
---
--- Table structure for table products
---
-CREATE TABLE products (
-  id INTEGER AUTO_INCREMENT NOT NULL UNIQUE,
-  product_name VARCHAR(20),
-  price DECIMAL(10,4),
-  stock_quantity INTEGER(11),
-  department_id INTEGER NOT NULL,
-  FOREIGN KEY (department_id) references departments (id),
-  PRIMARY KEY (id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Populate products table 
+-- Table structure for table `departments`
 --
-INSERT INTO products (id, product_name, price, stock_quantity, department_id) VALUES 
-(1, "Macbook Pro", 1500.0, 10, 1),
-(2, "iPad Pro", 899.95, 10, 1),
-(3, "Apple Pencil", 149.95, 10, 1),
-(4, "Amazon Echo", 500.0, 10, 1),
-(5, "Phantom Drone", 1999.95, 4, 1),
-(6, "Beats by Dre", 199.95, 20, 1),
-(7, "Windshield Wipers", 29.95, 40, 3),
-(8, "Synthetic Oil", 49.95, 40, 3),
-(9, "Jumper Cables", 69.95, 30, 3),
-(10, "License Plate Frame", 14.99, 30, 3),
-(11, "Keurig", 149.95, 40, 2),
-(12, "Refrigerator", 499.95, 20, 2),
-(13, "Coffee Grinder", 149.95, 40, 2),
-(14, "Carpet Cleaner", 299.95, 100, 2);
+
+DROP TABLE IF EXISTS `departments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `departments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `department_name` varchar(20) DEFAULT NULL,
+  `over_head_costs` decimal(10,4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table sales
+-- Dumping data for table `departments`
 --
-CREATE TABLE sales (
-  id INTEGER AUTO_INCREMENT NOT NULL UNIQUE,
-  product_id INTEGER NOT NULL,
-  quantity_purchased INTEGER,
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (product_id) references products(id),
-  PRIMARY KEY (id)
-);
 
+LOCK TABLES `departments` WRITE;
+/*!40000 ALTER TABLE `departments` DISABLE KEYS */;
+INSERT INTO `departments` VALUES (1,'Technology',8000.0000),(2,'Home Improvement',1000.0000),(3,'Automotive',10000.0000);
+/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(20) DEFAULT NULL,
+  `price` decimal(10,4) DEFAULT NULL,
+  `stock_quantity` int(11) DEFAULT NULL,
+  `department_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `department_id` (`department_id`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Macbook Pro',1500.0000,10,1),(2,'iPad Pro',899.9500,8,1),(3,'Apple Pencil',149.9500,10,1),(4,'Amazon Echo',500.0000,10,1),(5,'Phantom Drone',1999.9500,4,1),(6,'Beats by Dre',199.9500,20,1),(7,'Windshield Wipers',29.9500,40,3),(8,'Synthetic Oil',49.9500,38,3),(9,'Jumper Cables',69.9500,30,3),(10,'License Plate Frame',14.9900,30,3),(11,'Keurig',149.9500,38,2),(12,'Refrigerator',499.9500,16,2),(13,'Coffee Grinder',149.9500,38,2),(14,'Carpet Cleaner',299.9500,88,2);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sales`
+--
+
+DROP TABLE IF EXISTS `sales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sales` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `quantity_purchased` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sales`
+--
+
+LOCK TABLES `sales` WRITE;
+/*!40000 ALTER TABLE `sales` DISABLE KEYS */;
+INSERT INTO `sales` VALUES (1,11,2,'2017-02-25 04:15:05'),(2,12,2,'2017-02-25 04:18:53'),(3,12,2,'2017-02-25 04:19:40'),(4,14,2,'2017-02-25 04:24:24'),(5,14,2,'2017-02-25 04:31:12'),(6,14,2,'2017-02-25 04:46:58'),(7,14,2,'2017-02-25 04:51:03'),(8,14,2,'2017-02-25 04:52:08'),(9,14,2,'2017-02-25 04:54:42'),(10,8,2,'2017-02-25 05:32:55'),(11,2,1,'2017-02-25 05:37:31'),(12,2,1,'2017-02-25 05:38:51');
+/*!40000 ALTER TABLE `sales` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-25  1:48:36
